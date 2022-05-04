2fe3fcf113d4533a7063aaa9159c6d61054216e2
Micaela Frank <micaelalfrank@gmail.com>
2022-05-04 11:17:30 -0400

    commit tree 30845d7dcaa9f91dc3816c3bba2498642b113cba

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 // write your code here
+document.addEventListener('DOMContentLoaded', addRamenImages)
+
+function addRamenImages() {
+return fetch('http://localhost:3000/ramens')
+.then(response => response.json())
+.then(json => renderImage(json))
+
+
+function renderImage(ramens){
+    const parentDiv = document.querySelector('#ramen-menu')
+    let mainImage = document.querySelector('#ramen-detail img')
+    mainImage.id = "itsRamenTime"
+    let centerImage = document.querySelector('#itsRamenTime')
+    let restaurant = document.querySelector('.restaurant')
+    let nameOfRamen = document.querySelector('.name')
+
+    ramens.forEach(ramen => {
+        const img = document.createElement('img')
+        img.src = ramen.image
+        img.className = 'navbar-ramen-image'
+        img.id = ramen.id.toString()
+        parentDiv.appendChild(img)
+        
+        img.addEventListener('click', () => {
+        console.log(`yummy ${img.id}`)
+        centerImage = img.image
+        nameOfRamen = img.name
+        restaurant = img.restaurant
+    })
+}) 
+}
+}
